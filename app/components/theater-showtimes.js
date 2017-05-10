bad7b0724d4214e5d7e36a94f9320cb7caabf526
Josh Paul <jpaul@focusvision.com>
2017-05-09 23:50:02 -0700

    Center content (for desktop view). Increase image size (for mobile view).

--- app/components/theater-showtimes.js
+++ app/components/theater-showtimes.js
@@ -19,9 +19,9 @@ export default Ember.Component.extend({
   }),
   filteredMovies: Ember.computed('showtimes', 'movieFilter', 'movies', function() {
     if (this.get('movieFilter')) {
-      let movieFilter = this.get('movieFilter');
+      let movieFilter = this.get('movieFilter').toUpperCase();
       return this.get('movies').filter(function(movie) {
-        let nameValue = movie.movie.get('title');
+        let nameValue = movie.movie.get('title').toUpperCase();
        return nameValue.indexOf(movieFilter) >= 0;
       });
     } else {
--- app/models/theater.js
+++ app/models/theater.js
@@ -3,7 +3,6 @@ import DS from 'ember-data';
 var Theater = DS.Model.extend({
   name: DS.attr('string'),
   showtimes: DS.attr(),
-  // showTimes: DS.hasMany('showTime'),
   upName: Ember.computed('name', function() {
     return this.get('name').toUpperCase()
   })
